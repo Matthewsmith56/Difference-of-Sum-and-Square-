07cfc7beed0555e0566f688f9dd15abea04968ba
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-26 14:58:28 -0400

    Final Program

--- Sum Square Difference/Program.cs
+++ Sum Square Difference/Program.cs
@@ -10,6 +10,22 @@ namespace Sum_Square_Difference
     {
         static void Main(string[] args)
         {
+            int[] numsArray = new int[101];
+
+            for (int i = 1; i <= 100; i++)
+            {
+                numsArray[i] = i;
+            }
+
+            int totalOfsquare = 0;
+            int totalOfbase = 0;
+
+            foreach (var i in numsArray)
+            {
+                totalOfsquare += (i * i);
+                totalOfbase += i;
+            }
+            Console.WriteLine(totalOfbase * totalOfbase - totalOfsquare);
         }
     }
 }
